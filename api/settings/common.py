bbcb9f5cbb027c7a876196be260c88999b23b8d0
Paul M Furley <paul@paulfurley.com>
2014-06-13 17:06:21 +0100

    Modify timezone/language in settings/common.py

--- api/settings/common.py
+++ api/settings/common.py
@@ -66,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
