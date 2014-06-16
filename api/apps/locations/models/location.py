e7067df27ff20dd47860a4f53c0dd603dadd9cf9
Paul M Furley <paul@paulfurley.com>
2014-06-16 17:13:49 +0100

    Apply a strict validator to slug field

--- api/apps/locations/models/location.py
+++ api/apps/locations/models/location.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+STRICT_SLUG_VALIDATOR = RegexValidator(
+    r'^[0-9a-z-]+$',
+    'Only lowercase alphanumeric characters and hyphens are allowed.')
 
 
 class Location(models.Model):
     class Meta:
         app_label = 'locations'
 
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        validators=[STRICT_SLUG_VALIDATOR])
