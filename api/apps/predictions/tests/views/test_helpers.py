72ba1774f361547e65d85f55fa568cf9eb17789f
Paul M Furley <paul@paulfurley.com>
2014-06-23 14:58:58 +0100

    Wrote `split_prediction_windows` function & tests
    Given a list of Predictions, containing multiple tidal windows, split
    into the start & end prediction of each window.

---
+++ api/apps/predictions/tests/views/test_helpers.py
@@ -0,0 +1,87 @@
+import datetime
+import pytz
+
+from nose.tools import assert_equal, assert_raises
+
+from django.test import TestCase
+
+from api.apps.predictions.views.helpers import split_prediction_windows
+
+
+class FakePrediction(object):
+    def __init__(self, minute):
+        self.datetime = TestSplitPrediction.BASE_DATETIME.replace(
+            minute=minute)
+
+
+class TestSplitPrediction(TestCase):
+    BASE_DATETIME = datetime.datetime(2013, 1, 1, 10, 0, 0, tzinfo=pytz.UTC)
+
+    @classmethod
+    def _to_mins(cls, dt):
+        return (dt - cls.BASE_DATETIME).total_seconds() / 60
+
+    def _split_these_minutes(self, minutes):
+        predictions = [FakePrediction(m) for m in minutes]
+        return [(self._to_mins(start.datetime), self._to_mins(end.datetime))
+                for start, end in split_prediction_windows(predictions)]
+
+    def test_split_prediction_windows_splits_single_window(self):
+        assert_equal([
+            (2, 4),
+        ],
+            self._split_these_minutes([2, 3, 4])
+        )
+
+    def test_that_it_splits_window_with_only_two_predictions(self):
+        assert_equal([
+            (2, 3),
+        ],
+            self._split_these_minutes([2, 3])
+        )
+
+    def test_that_it_splits_multiple_windows_correctly(self):
+        assert_equal([
+            (2, 4),
+            (10, 12),
+            (25, 26),
+        ],
+            self._split_these_minutes([2, 3, 4, 10, 11, 12, 25, 26])
+        )
+
+    def test_that_it_ignores_single_predictions_at_start(self):
+        assert_equal([
+            (10, 13),
+        ],
+            self._split_these_minutes([3, 10, 11, 12, 13])
+        )
+
+    def test_that_it_ignores_single_predictions_at_end(self):
+        assert_equal([
+            (10, 13),
+        ],
+            self._split_these_minutes([10, 11, 12, 13, 34])
+        )
+
+    def test_that_it_ignores_single_predictions_in_middle(self):
+        assert_equal([
+            (10, 12),
+            (31, 33),
+        ],
+            self._split_these_minutes([10, 11, 12, 20, 31, 32, 33])
+        )
+
+    def test_that_it_blows_up_if_predictions_dont_increase(self):
+        assert_raises(
+            ValueError,
+            lambda: self._split_these_minutes([5, 6, 7, 7, 8, 9])
+        )
+
+    def test_that_it_blows_up_if_predictions_decrease_in_time(self):
+        assert_raises(
+            ValueError,
+            lambda: self._split_these_minutes([5, 6, 7, 6, 7, 8])
+        )
+
+    def test_that_it_handles_empty_predictions(self):
+        assert_equal([], self._split_these_minutes([]))
--- api/apps/predictions/views/helpers.py
+++ api/apps/predictions/views/helpers.py
@@ -1,6 +1,7 @@
 import datetime
 
 from collections import namedtuple
+from itertools import tee, izip
 from urlparse import urlparse, urlunparse, parse_qsl
 from urllib import urlencode
 
@@ -56,6 +57,57 @@ def parse_time_range(start, end):
 def parse_datetime(datetime_string):
     return datetime.datetime.strptime(
         datetime_string, DATETIME_FORMAT).replace(tzinfo=pytz.UTC)
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
+
+
+def split_prediction_windows(predictions):
+    """
+    `predictions` contains all the times for which the tide is above a certain
+    level, eg, if there are two tidal windows, it will contain every minutely
+    prediction for the first, followed by every prediction for the second.
+
+    This function yield the start and end prediction of each time window:
+    based on discontinuities in time (ie a gap larger than one minue)
+
+    a0, a1, a2, a3, a0, b0, b1, b2, b3  => (a0, a3), (b0, b3)
+    """
+    def _remove_unequal(pair):
+        return pair[0] != pair[1]
+
+    return filter(
+        _remove_unequal,
+        _split_prediction_windows_unfiltered(predictions)
+    )
+
+
+def _split_prediction_windows_unfiltered(predictions):
+    if not len(predictions):
+        return
+
+    one_minute = datetime.timedelta(seconds=60)
+
+    window_start = predictions[0]
+    last_prediction_seen = None
+
+    for p0, p1 in pairwise(predictions):
+        timediff = p1.datetime - p0.datetime
+
+        if timediff > one_minute:
+            yield window_start, p0
+            window_start = p1
+
+        elif p0.datetime >= p1.datetime:
+            raise ValueError("Predictions must be ordered and ascending.")
+
+        last_prediction_seen = p1
+
+    yield window_start, last_prediction_seen
 
 
 def add_start_of_now_to_url(url):
