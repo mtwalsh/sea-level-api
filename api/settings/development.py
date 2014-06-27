f0e5203e499b0a4b46301860d70ff0a206039057
Paul M Furley <paul@paulfurley.com>
2014-06-27 11:21:45 +0100

    Allow disabling DEBUG in development
    It's really useful to be able to turn off to mirror production
    behaviour, ie
    
    ```
    DEBUG=false python manage.py runserver
    ```

--- api/settings/development.py
+++ api/settings/development.py
@@ -3,7 +3,8 @@ from .common import *
 if SECRET_KEY is None:
     SECRET_KEY = 'insecure-secret-key-only-for-development'
 
-DEBUG = TEMPLATE_DEBUG = True  # SECURITY WARNING: insecure! leaks secrets.
+if 'true' == os.environ.get('DEBUG', 'true'):  # default on but allow disabling
+    DEBUG = TEMPLATE_DEBUG = True  # SECURITY WARNING: insecure! leaks secrets.
 
 if DATABASES is None:
     print("Using SQLite database.")
