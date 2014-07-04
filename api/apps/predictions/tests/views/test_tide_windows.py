169d009767510df281f91a03c6731e08820322bc
Paul M Furley <paul@paulfurley.com>
2014-07-04 17:46:47 +0100

    Add (failing) tests for extend windows code
    [#74438722]

--- api/apps/predictions/tests/views/test_tide_windows.py
+++ api/apps/predictions/tests/views/test_tide_windows.py
@@ -17,11 +17,43 @@ from .test_time_parsing import TimeParsingTestMixin
 class TestTideWindowsViewBase(TestCase):
     PATH = '/1/predictions/tide-windows/'
 
+    fixtures = [
+        'api/apps/locations/fixtures/two_locations.json',
+    ]
+
+    @classmethod
+    def setUp(cls):
+        cls.location = Location.objects.get(slug='liverpool')
+        cls.base_time = datetime.datetime(2014, 6, 1, 10, 00, tzinfo=pytz.UTC)
+
+    @classmethod
+    def create_predictions(cls, minutes_and_levels):
+        for minute, level in minutes_and_levels:
+            Prediction.objects.create(
+                location=cls.location,
+                datetime=cls.base_time + datetime.timedelta(minutes=minute),
+                tide_level=level
+            )
+
+    @staticmethod
+    def parse_window(data):
+        return (
+            data['start']['datetime'],
+            data['start']['tide_level'],
+            data['end']['datetime'],
+            data['end']['tide_level'],
+            data['duration']['total_seconds']
+        )
+
+    def get_tide_windows(self, path):
+        response = self.client.get(self.PATH + path)
+        data = decode_json(response.content)
+        return data['tide_windows']
+
 
 class TestTideWindowsView(TestTideWindowsViewBase, LocationParsingTestMixin,
                           TimeParsingTestMixin):
-    fixtures = [
-        'api/apps/locations/fixtures/two_locations.json',
+    fixtures = TestTideWindowsViewBase.fixtures + [
         'api/apps/predictions/fixtures/predictions_two_locations.json',
     ]
 
@@ -69,22 +101,11 @@ class TestTideWindowsView(TestTideWindowsViewBase, LocationParsingTestMixin,
         assert_equal(expected, tide_windows[0])
 
 
-class TestTideWindowsCalculationsView(TestTideWindowsViewBase):
-    fixtures = [
-        'api/apps/locations/fixtures/two_locations.json',
-    ]
-
+class TestTideWindowsSimpleCalculations(TestTideWindowsViewBase):
     @classmethod
     def setUp(cls):
-        cls.create_double_peaked_tide()
-
-    @classmethod
-    def create_double_peaked_tide(cls):
-        location = Location.objects.get(slug='liverpool')
-
-        cls.base_time = datetime.datetime(2014, 6, 1, 10, 00, tzinfo=pytz.UTC)
-
-        for minute, level in [
+        super(TestTideWindowsSimpleCalculations, cls).setUp()
+        cls.create_predictions([
             (0, 4.50),
             (1, 4.75),
             (2, 5.00),
@@ -102,84 +123,243 @@ class TestTideWindowsCalculationsView(TestTideWindowsViewBase):
             (14, 5.00),
             (15, 4.75),
             (16, 4.50)
-        ]:
-
-            Prediction.objects.create(
-                location=location,
-                datetime=cls.base_time + datetime.timedelta(minutes=minute),
-                tide_level=level
-            )
+        ])
 
     def test_that_single_window_is_correctly_identified(self):
-        response = self.client.get(
-            self.PATH + 'liverpool/'
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
             '?start=2014-06-01T10:00:00Z'
             '&end=2014-06-02T11:00:00Z'
-            '&tide_level=5.5'
-        )
-        data = decode_json(response.content)
-        assert_equal([
-            {
-                'start': {
-                    'datetime': '2014-06-01T10:04:00Z',
-                    'tide_level': 5.50,
-                },
-                'end': {
-                    'datetime': '2014-06-01T10:07:00Z',
-                    'tide_level': 5.60,
-                },
-                'duration': {
-                    'total_seconds': 240,
-                }
-            }],
-            data['tide_windows']
-        )
+            '&tide_level=5.5')
+        assert_equal(1, len(tide_windows))
+        assert_equal(
+            ('2014-06-01T10:04:00Z', 5.50, '2014-06-01T10:07:00Z', 5.60, 240),
+            self.parse_window(tide_windows[0]))
 
     def test_that_double_window_is_correctly_identified(self):
-        response = self.client.get(
-            self.PATH + 'liverpool/'
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
             '?start=2014-06-01T10:00:00Z'
             '&end=2014-06-02T11:00:00Z'
-            '&tide_level=5.1'
-        )
-        data = decode_json(response.content)
-        assert_equal([
-            {
-                'start': {
-                    'datetime': '2014-06-01T10:03:00Z',
-                    'tide_level': 5.25,
-                },
-                'end': {
-                    'datetime': '2014-06-01T10:09:00Z',
-                    'tide_level': 5.25,
-                },
-                'duration': {
-                    'total_seconds': 420,
-                }
-            },
-            {
-                'start': {
-                    'datetime': '2014-06-01T10:11:00Z',
-                    'tide_level': 5.25,
-                },
-                'end': {
-                    'datetime': '2014-06-01T10:13:00Z',
-                    'tide_level': 5.25,
-                },
-                'duration': {
-                    'total_seconds': 180,
-                }
-            },
-        ],
-            data['tide_windows']
-        )
+            '&tide_level=5.1')
+        assert_equal(2, len(tide_windows))
+
+        assert_equal(
+            ('2014-06-01T10:03:00Z', 5.25, '2014-06-01T10:09:00Z', 5.25, 420),
+            self.parse_window(tide_windows[0]))
+
+        assert_equal(
+            ('2014-06-01T10:11:00Z', 5.25, '2014-06-01T10:13:00Z', 5.25, 180),
+            self.parse_window(tide_windows[1]))
 
     def test_that_no_tidal_window_returned_if_tide_is_never_above_height(self):
-        response = self.client.get(
-            self.PATH + 'liverpool/'
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
             '?start=2014-06-01T10:00:00Z'
             '&end=2014-06-02T11:00:00Z'
-            '&tide_level=6.1'
-        )
-        data = decode_json(response.content)
-        assert_equal([], data['tide_windows'])
+            '&tide_level=6.1')
+        assert_equal([], tide_windows)
+
+
+class TestTideWindowsOverlappingStartTime(TestTideWindowsViewBase):
+    def test_that_a_tide_window_overlapping_start_time_has_correct_start(self):
+        #                   start      end
+        #                    |          |
+        #                /------\
+        #
+        # expected:      ^      ^
+        self.create_predictions([
+            (-4, 4),
+            (-3, 5),
+            (-2, 6),
+            (-1, 6),
+            (0, 6),
+            (1, 6),
+            (2, 6),
+            (3, 6),
+            (4, 5),
+            (5, 4),
+        ])
+
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
+            '?start=2014-06-01T10:00:00Z'
+            '&end=2014-06-02T11:00:00Z'
+            '&tide_level=5')
+        assert_equal(1, len(tide_windows))
+
+        assert_equal(
+            ('2014-06-01T09:57:00Z', 5.0, '2014-06-01T10:04:00Z', 5.0, 480),
+            self.parse_window(tide_windows[0]))
+
+
+class TestTideWindowsOverlappingEndTime(TestTideWindowsViewBase):
+    def test_that_a_tide_window_overlapping_end_time_has_correct_end(self):
+        #       start      end
+        #        |          |
+        #                /------\
+        #
+        # expected:      ^      ^
+
+        self.create_predictions([
+            (0, 4),
+            (1, 5),
+            (2, 6),
+            (3, 6),
+            (4, 6),
+            (5, 6),
+            (6, 6),
+            (7, 6),
+            (8, 5),
+            (9, 4),
+        ])
+
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
+            '?start=2014-06-01T10:00:00Z'
+            '&end=2014-06-01T10:05:00Z'
+            '&tide_level=5')
+        assert_equal(1, len(tide_windows))
+
+        assert_equal(
+            ('2014-06-01T10:01:00Z', 5, '2014-06-01T10:08:00Z', 5, 480),
+            self.parse_window(tide_windows[0]))
+
+
+class TestTideWindowsFullyBeforeStart(TestTideWindowsViewBase):
+    def test_that_a_tide_window_fully_before_start_is_ignored(self):
+        # start-24h    start      end
+        #   |            |          |
+        #      /----\
+        #
+        # expected:      no windows
+
+        self.create_predictions([
+            (-10, 6),
+            (-9, 6),
+            (-8, 6),
+            (-7, 6),
+            (-6, 6),
+            (-5, 6),
+            (-4, 2),
+            (-3, 2),
+            (-2, 2),
+            (-1, 2),
+        ])
+
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
+            '?start=2014-06-01T10:00:00Z'
+            '&end=2014-06-01T10:05:00Z'
+            '&tide_level=5')
+        assert_equal(0, len(tide_windows))
+
+
+class TestTideWindowsFullyAfterEnd(TestTideWindowsViewBase):
+    def test_that_a_tide_window_fully_after_end_is_ignored(self):
+        # start      end          end+24h
+        #   |          |           |
+        #                  /----\
+        #
+        # expected:      no windows
+
+        self.create_predictions([
+            (10, 6),
+            (11, 6),
+            (12, 6),
+            (13, 6),
+            (14, 6),
+            (15, 6),
+        ])
+
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
+            '?start=2014-06-01T10:00:00Z'
+            '&end=2014-06-01T10:05:00Z'
+            '&tide_level=5')
+        assert_equal(0, len(tide_windows))
+
+
+class TestTideWindowsExtendingLongPastStart(TestTideWindowsViewBase):
+    def test_that_a_tide_window_going_24_hours_before_start_is_squashed(self):
+        # start-24h     start       end
+        #   |             |          |
+        # -------------------\
+        #
+        # expected:       ^  ^
+
+        self.create_predictions(
+            (minute, 6) for minute in range(-25 * 60, 0, 1))
+
+        self.create_predictions([
+            (0, 6),
+            (1, 6),
+            (2, 6),
+            (3, 6),
+            (4, 5),
+            (5, 4),
+            (6, 3),
+        ])
+
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
+            '?start=2014-06-01T10:00:00Z'
+            '&end=2014-06-01T12:00:00Z'
+            '&tide_level=6')
+        assert_equal(1, len(tide_windows))
+
+        assert_equal(
+            ('2014-06-01T10:00:00Z', 6.0, '2014-06-01T10:03:00Z', 6.0, 240),
+            self.parse_window(tide_windows[0]))
+
+
+class TestTideWindowsExtendingLongPastEnd(TestTideWindowsViewBase):
+    def test_that_a_tide_window_going_24_hours_after_end_is_squashed(self):
+        #               start       end          end+24hours
+        #                 |          |             |
+        #                        /--------------------------
+        #
+        # expected:              ^   ^
+
+        self.create_predictions([
+            (0, 3),
+            (1, 6),
+            (2, 6),
+            (3, 6),
+            (4, 6),
+        ])
+
+        self.create_predictions(
+            (minute, 6) for minute in range(5, 5 + 25 * 600, 1))
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
+            '?start=2014-06-01T10:00:00Z'
+            '&end=2014-06-01T10:04:00Z'
+            '&tide_level=6')
+        assert_equal(1, len(tide_windows))
+
+        assert_equal(
+            ('2014-06-01T10:01:00Z', 6.0, '2014-06-01T10:04:00Z', 6.0, 240),
+            self.parse_window(tide_windows[0]))
+
+
+class TestTideWindowsExtendingLongPastStartAndEnd(TestTideWindowsViewBase):
+    def test_that_a_tide_thats_always_above_start_and_end_is_squashed(self):
+        # start-24h     start       end          end+24hours
+        #  |              |          |             |
+        # /--------------------------------------------------\
+        #
+        # expected:       ^          ^
+
+        self.create_predictions(
+            (minute, 6) for minute in range(-25 * 600, 5 + 25 * 600, 1))
+        tide_windows = self.get_tide_windows(
+            'liverpool/'
+            '?start=2014-06-01T10:00:00Z'
+            '&end=2014-06-01T10:05:00Z'
+            '&tide_level=6')
+        assert_equal(1, len(tide_windows))
+
+        assert_equal(
+            ('2014-06-01T10:00:00Z', 6.0, '2014-06-01T10:05:00Z', 6.0, 360),
+            self.parse_window(tide_windows[0]))
