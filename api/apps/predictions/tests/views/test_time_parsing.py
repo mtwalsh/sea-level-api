635f4f36ea60a1757a8c7a536f805396e18b6d7a
Paul M Furley <paul@paulfurley.com>
2014-07-08 22:34:15 +0100

    Make `start` and `end` required, add tests
    This also removes the remaining "skipped" tests which is a Good Thing.
    
    https://www.pivotaltracker.com/story/show/74643936
    
    [Fixes #74643936]

--- api/apps/predictions/tests/views/test_time_parsing.py
+++ api/apps/predictions/tests/views/test_time_parsing.py
@@ -1,10 +1,22 @@
 from nose.tools import assert_equal
-from nose.plugins.skip import SkipTest
+
+from api.libs.test_utils import decode_json
 
 
 class TimeParsingTestMixin(object):
 
-    def test_that_no_start_and_end_parameter_temporary_redirects_to_now(self):
-        raise SkipTest("Not yet implemented.")
-        response = self.client.get(self.PATH + 'liverpool/')
-        assert_equal(302, response.status_code)
+    def test_that_no_start_parameter_is_an_error(self):
+        response = self.client.get(
+            self.PATH + 'liverpool/?end=2014-01-01T00:00:00Z')
+        assert_equal(400, response.status_code)
+        assert_equal(
+            'Missing parameter `start`. Format: 2014-11-30T00:00:00Z',
+            decode_json(response.content)['detail'])
+
+    def test_that_no_end_parameter_is_an_error(self):
+        response = self.client.get(
+            self.PATH + 'liverpool/?start=2014-01-01T00:00:00Z')
+        assert_equal(400, response.status_code)
+        assert_equal(
+            'Missing parameter `end`. Format: 2014-11-30T00:00:00Z',
+            decode_json(response.content)['detail'])
