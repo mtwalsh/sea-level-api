38b2245713616f67aa4ca5e77b01ffcda6662a46
Paul M Furley <paul@paulfurley.com>
2014-07-31 18:16:29 +0100

    Management command to import latest EA measurement
    Using ea-measurement-collector which is installed into api.libs.
    
    Now you can add a station to the database then run:
    ```
    python manage.py get_latest_ea_measurements
    ```
    
    And the latest measurement will be fetched and inserted into the
    database for each station.
    
    https://www.pivotaltracker.com/story/show/75972824
    [Delivers #75972824]

---
+++ api/apps/ea_sea_levels/management/commands/get_latest_ea_measurements.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+
+from api.apps.ea_sea_levels.models import Station, Measurement
+
+from api.libs.ea_measurement_collector import download_latest_measurement
+
+
+class Command(BaseCommand):
+    docs = "Usage: get_latest_ea_measurements"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for station in Station.objects.all():
+            self.stdout.write("Getting measurement for {}".format(station))
+            result = download_latest_measurement(
+                station.station_id)
+
+        validate_datum(result.datum, station.site_datum)
+        validate_station(result.station_id, station.station_id)
+        self.record_measurement(station, result)
+
+    def record_measurement(self, station, result):
+        self.stdout.write("parse result: {}".format(result))
+        obj, created = Measurement.objects.update_or_create(
+            station=station,
+            datetime=result.datetime,
+            defaults={'measurement': result.measurement})
+        if created:
+            self.stdout.write("Saved new measurement.")
+        else:
+            self.stdout.write("Updated measurement.")
+
+
+def validate_datum(reported_datum, site_datum):
+    assert reported_datum == site_datum
+
+
+def validate_station(reported_station_id, station_id):
+    assert reported_station_id == station_id
