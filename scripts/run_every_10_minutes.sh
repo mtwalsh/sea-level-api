f893fc35655135b9b903da77f9367b12485b836d
Paul M Furley <paul@paulfurley.com>
2014-08-05 11:34:39 +0100

    Convert measurements every 10 minutes

--- scripts/run_every_10_minutes.sh
+++ scripts/run_every_10_minutes.sh
@@ -4,8 +4,13 @@ function download_ea_measurements {
   python manage.py get_latest_ea_measurements
 }
 
+function convert_ea_measurements_into_observations {
+  python manage.py convert_measurements
+}
+
 pushd $(dirname $0)/..
 
 download_ea_measurements
+convert_ea_measurements_into_observations
 
 popd
