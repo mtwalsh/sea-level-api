cca8f1dd110b01553b0e4e30204411c26c8f080a
Paul M Furley <paul@paulfurley.com>
2014-08-13 13:00:57 +0100

    Add database model CombinedPredictionObservation
    This uses a custom SQL view written in the initial migration. The Django
    model is "unmanaged" meaning it doesn't try to modify the database.
    
    [#75795976]

---
+++ api/apps/sea_levels/models/combined_prediction_observation.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+from api.apps.locations.models import Location
+
+
+@python_2_unicode_compatible
+class CombinedPredictionObservation(models.Model):
+    class Meta:
+        app_label = 'sea_levels'
+        managed = False
+
+    datetime = models.DateTimeField(primary_key=True)
+    location = models.ForeignKey(Location, null=False)
+    predicted_tide_level = models.FloatField(null=False)
+    observed_sea_level = models.FloatField(null=True)
+    derived_surge_level = models.FloatField(null=True)
+
+    def __str__(self):
+        return "{}".format(self.datetime)
