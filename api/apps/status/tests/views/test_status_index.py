3236cc372694f833917f282f0927554aecdaf85e
Paul M Furley <paul@paulfurley.com>
2014-09-01 15:24:58 +0100

    Add monitoring for surge prediction freshness
    We must have 12 hours of continuous surge model predictions for the
    check to pass.
    
    https://www.pivotaltracker.com/story/show/74244296 [Delivers #74244296]

--- api/apps/status/tests/views/test_status_index.py
+++ api/apps/status/tests/views/test_status_index.py
@@ -9,16 +9,29 @@ from nose.tools import assert_equal
 
 from api.apps.observations.models import Observation
 from api.apps.observations.utils import create_observation
-from api.apps.predictions.models import TidePrediction
-from api.apps.predictions.utils import create_tide_prediction
+from api.apps.predictions.models import TidePrediction, SurgePrediction
+from api.apps.predictions.utils import (
+    create_tide_prediction, create_surge_prediction)
 
 from api.apps.locations.models import Location
 
 from api.apps.status.views.status_index import (
-    check_tide_predictions, check_observations)
+    check_tide_predictions, check_observations, check_surge_predictions)
 
 
-class TestStatusIndexView(TestCase):
+class MakeSurgePredictionsMixin(object):
+    def _make_good_surge_predictions(self):
+        liverpool, _ = Location.objects.get_or_create(
+            slug='liverpool', name='Liverpool')
+
+        for minute in range((12 * 60) + 10):
+            create_surge_prediction(
+                liverpool,
+                self.base_time + datetime.timedelta(minutes=minute),
+                0.2)
+
+
+class TestStatusIndexView(TestCase, MakeSurgePredictionsMixin):
     BASE_PATH = '/1/_status/'
 
     def setUp(self):
@@ -31,6 +44,7 @@ class TestStatusIndexView(TestCase):
             self.liv,
             self.base_time + datetime.timedelta(days=31),
             5.0)
+        self._make_good_surge_predictions()
         create_observation(
             self.liv,
             self.base_time - datetime.timedelta(minutes=10),
@@ -39,6 +53,7 @@ class TestStatusIndexView(TestCase):
 
     def _setup_not_ok(self):
         TidePrediction.objects.all().delete()
+        SurgePrediction.objects.all().delete()
         Observation.objects.all().delete()
 
     def test_that_status_page_has_api_status_ok_when_all_ok(self):
@@ -140,5 +155,32 @@ class TestCheckObservations(TestCheckBase):
         assert_equal('> 1 hour old', text)
 
 
-class TestCheckSurgePredictions(TestCheckBase):
-    pass
+class TestCheckSurgePredictions(TestCheckBase, MakeSurgePredictionsMixin):
+    def setUp(self):
+        super(TestCheckSurgePredictions, self).setUp()
+        self._make_good_surge_predictions()
+
+    def test_that_surge_predictions_for_next_12_hours_every_minute_is_ok(self):
+        with freeze_time(self.base_time):
+            (ok, text) = check_surge_predictions(self.liverpool)
+
+        assert_equal('OK', text)
+        assert_equal(True, ok)
+
+    def test_that_a_missing_surge_prediction_in_next_12_hours_not_ok(self):
+        prediction = SurgePrediction.objects.get(
+            location=self.liverpool,
+            minute__datetime=self.base_time + datetime.timedelta(minutes=10))
+        prediction.delete()
+
+        with freeze_time(self.base_time):
+            (ok, text) = check_surge_predictions(self.liverpool)
+
+        assert_equal(False, ok)
+        assert_equal('Missing data for next 12 hours: 719 vs 720', text)
+
+    def test_that_predictions_for_liverpool_dont_affect_southampton(self):
+        with freeze_time(self.base_time):
+            (ok, text) = check_surge_predictions(self.southampton)
+
+        assert_equal(False, ok)
