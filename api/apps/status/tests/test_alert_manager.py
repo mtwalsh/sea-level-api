24c6358d8b6b3caa17c08cf9bef343dad4f33d24
Paul M Furley <paul@paulfurley.com>
2014-11-28 15:27:24 +0000

    Fix tests in status module
    We should always use freeze_time when testing a "disable" or "check
    disabled" function in the alert manager.
    
    Also clean up before and after each test.
    
    https://www.pivotaltracker.com/story/show/83590686 [Delivers #83590686]

--- api/apps/status/tests/test_alert_manager.py
+++ api/apps/status/tests/test_alert_manager.py
@@ -18,15 +18,23 @@ BASE_DATE = datetime.datetime(2014, 11, 24, 10, 0, tzinfo=pytz.UTC)
 
 class TestAlertManagerBase(TestCase):
     def setUp(self):
+        self._clean_up()
         self.liverpool = Location.objects.create(slug='liverpool')
 
     def tearDown(self):
+        self._clean_up()
+
+    def _clean_up(self):
         Location.objects.all().delete()
         LocationStatusConfig.objects.all().delete()
 
     def _disable(self, alert_type):
         disable_alert_until(self.liverpool, alert_type,
                             BASE_DATE + datetime.timedelta(hours=10))
+
+    def _disable_all_alerts(self):
+        for alert_type in AlertType:
+            self._disable(alert_type)
 
     def _enable(self, alert_type):
         enable_alert(self.liverpool, alert_type)
@@ -42,78 +50,77 @@ class TestAlertManagerBase(TestCase):
 
 class TestAlertManagerEnablingDisabling(TestAlertManagerBase):
 
+    @freeze_time(BASE_DATE)
     def test_that_disable_alert_until__cant_be_called_with_past_datetime(self):
-        with freeze_time(BASE_DATE):
-            assert_raises(
-                AssertionError,
-                lambda: disable_alert_until(
-                    self.liverpool, AlertType.tide_predictions,
-                    BASE_DATE - datetime.timedelta(hours=1)))
+        assert_raises(
+            AssertionError,
+            lambda: disable_alert_until(
+                self.liverpool, AlertType.tide_predictions,
+                BASE_DATE - datetime.timedelta(hours=1)))
 
+    @freeze_time(BASE_DATE)
     def test_tide_predictions_alerts_can_be_disabled(self):
         self._assert_enabled(AlertType.tide_predictions)
 
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.tide_predictions)
-            self._assert_disabled(AlertType.tide_predictions)
+        self._disable(AlertType.tide_predictions)
+        self._assert_disabled(AlertType.tide_predictions)
 
+    @freeze_time(BASE_DATE)
     def test_surge_predictions_alerts_can_be_disabled(self):
         self._assert_enabled(AlertType.surge_predictions)
 
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.surge_predictions)
-            self._assert_disabled(AlertType.surge_predictions)
+        self._disable(AlertType.surge_predictions)
+        self._assert_disabled(AlertType.surge_predictions)
 
+    @freeze_time(BASE_DATE)
     def test_observations_alerts_can_be_disabled(self):
         self._assert_enabled(AlertType.observations)
 
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.observations)
-            self._assert_disabled(AlertType.observations)
+        self._disable(AlertType.observations)
+        self._assert_disabled(AlertType.observations)
 
+    @freeze_time(BASE_DATE)
     def test_that_tide_predictions_alerts_can_be_re_enabled(self):
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.tide_predictions)
+        self._disable(AlertType.tide_predictions)
 
         self._enable(AlertType.tide_predictions)
         self._assert_enabled(AlertType.tide_predictions)
 
+    @freeze_time(BASE_DATE)
     def test_that_surge_predictions_alerts_can_be_re_enabled(self):
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.surge_predictions)
+        self._disable(AlertType.surge_predictions)
 
         self._enable(AlertType.surge_predictions)
         self._assert_enabled(AlertType.surge_predictions)
 
+    @freeze_time(BASE_DATE)
     def test_that_observations_alerts_can_be_re_enabled(self):
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.observations)
+        self._disable(AlertType.observations)
 
         self._enable(AlertType.observations)
         self._assert_enabled(AlertType.observations)
 
+    @freeze_time(BASE_DATE)
     def test_that__enable_all_alerts__actually_enables_all_alerts(self):
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.tide_predictions)
-            self._disable(AlertType.surge_predictions)
-            self._disable(AlertType.observations)
+        self._disable_all_alerts()
+
         enable_all_alerts(self.liverpool)
 
         self._assert_enabled(AlertType.tide_predictions)
         self._assert_enabled(AlertType.surge_predictions)
         self._assert_enabled(AlertType.observations)
 
+    @freeze_time(BASE_DATE)
     def test_that__alerts_disabled___returns_correct_set(self):
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.tide_predictions)
+        self._disable(AlertType.tide_predictions)
 
         assert_equal(
             set([AlertType.tide_predictions]),
             alerts_disabled(self.liverpool))
 
+    @freeze_time(BASE_DATE)
     def test_that__alerts_enabled__returns_correct_set(self):
-        with freeze_time(BASE_DATE):
-            self._disable(AlertType.tide_predictions)
+        self._disable(AlertType.tide_predictions)
 
         assert_equal(
             set([AlertType.surge_predictions, AlertType.observations]),
@@ -133,6 +140,7 @@ class TestAlertManagerImplementation(TestAlertManagerBase):
         is_alert_enabled(self.liverpool, AlertType.tide_predictions)
         self._assert_has_config()
 
+    @freeze_time(BASE_DATE)
     def test_that__disable_alert_until__works_without_prior_config(self):
         self._assert_no_config()
         self._disable(AlertType.tide_predictions)
@@ -148,6 +156,7 @@ class TestAlertManagerImplementation(TestAlertManagerBase):
         enable_all_alerts(self.liverpool)
         self._assert_has_config()
 
+    @freeze_time(BASE_DATE)
     def test_that_none_value_for__disabled_until__is_ok(self):
         LocationStatusConfig.objects.create(
             location=self.liverpool,
@@ -156,12 +165,14 @@ class TestAlertManagerImplementation(TestAlertManagerBase):
             True,
             is_alert_enabled(self.liverpool, AlertType.tide_predictions))
 
+    @freeze_time(BASE_DATE)
     def test_that_disabled_alerts_sets__disabled_until__to_datetime(self):
         self._disable(AlertType.tide_predictions)
         config = LocationStatusConfig.objects.get(location__slug='liverpool')
         assert_is_instance(config.tide_predictions_alerts_disabled_until,
                            datetime.datetime)
 
+    @freeze_time(BASE_DATE)
     def test_that_enable_alerts_sets__disabled_until__to_none(self):
         self._disable(AlertType.tide_predictions)
         self._enable(AlertType.tide_predictions)
